07a2f6d08a17d06b15259471f155e642842bcf26
pts5625 <nobin5625@gmail.com>
2025-07-23 08:16:28 +0000

    app.py has been added

---
+++ app.py
@@ -0,0 +1,110 @@
+'''
+import pandas as pd
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+import mlflow
+import mlflow.sklearn
+
+from sklearn.metrics import mean_squared_error
+
+def read_dataframe(filename):
+    df = pd.read_csv(filename)
+
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
+
+df_train = read_dataframe('./data/green_tripdata_2025-01.csv')
+df_val = read_dataframe('./data/green_tripdata_2024-06.csv')
+
+df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
+
+categorical = ['PU_DO'] #'PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+dv = DictVectorizer()
+
+train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+X_train = dv.fit_transform(train_dicts)
+
+val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+X_val = dv.transform(val_dicts)
+
+target = 'duration'
+y_train = df_train[target].values
+y_val = df_val[target].values
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+mean_squared_error(y_val, y_pred, squared=False)
+
+with mlflow.start_run():
+
+    mlflow.set_tag("developer", "cristian")
+
+    mlflow.log_param("train-data-path", "./data/green_tripdata_2021-01.csv")
+    mlflow.log_param("valid-data-path", "./data/green_tripdata_2021-02.csv")
+
+    alpha = 0.1
+    mlflow.log_param("alpha", alpha)
+    lr = Lasso(alpha)
+    lr.fit(X_train, y_train)
+
+    y_pred = lr.predict(X_val)
+    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    mlflow.log_metric("rmse", rmse)
+
+    mlflow.log_artifact(local_path="models/lin_reg.bin", artifact_path="models_pickle")
+
+'''
+import pandas as pd
+df= pd.read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-02.parquet')
+print(df.head())
+
+df['duration']=df['tpep_dropoff_datetime']-df['tpep_pickup_datetime']
+df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+df=df.loc[((df.duration > 25) & (df.duration <50)) & ((df.trip_distance >4) & (df.trip_distance <14))]
+df.dropna(inplace=True)
+
+from sklearn.model_selection import train_test_split
+
+# imprt elasticnet
+from sklearn.linear_model import ElasticNet
+parameters= {
+    'alpha': 0.1,
+    'l1_ratio': 0.5
+}
+model= ElasticNet(
+    alpha=parameters['alpha'],
+    l1_ratio=parameters['l1_ratio']
+)
+
+X = df[['passenger_count', 'trip_distance','duration']]
+y = df['total_amount']
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_val)
+from sklearn.metrics import root_mean_squared_error
+rmse = root_mean_squared_error(y_val, y_pred, squared=False)
+print(f"RMSE: {rmse}")
+
